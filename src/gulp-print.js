8265e0687297bda99522d912b7a5fb3f3e078785
decaffeinate <alex.gorbatchev@gmail.com>
2017-10-10 16:27:13 -0700

    decaffeinate: Convert gulp-print.coffee to JS

--- src/gulp-print.js
+++ src/gulp-print.js
@@ -1,18 +1,27 @@
-map = require 'map-stream'
-path = require 'path'
-log = require 'fancy-log'
-colors = require 'ansi-colors'
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const map = require('map-stream');
+const path = require('path');
+const {log, colors} = require('gulp-util');
+const log = require('fancy-log');
+const colors = require('ansi-colors');
 
-{green, magenta} = colors
+const {green, magenta} = colors;
 
-print = (format) ->
-  format ?= (filepath) -> filepath
+var print = function(format) {
+  if (format == null) { format = filepath => filepath; }
 
-  map (file, cb) ->
-    filepath = magenta(path.relative(process.cwd(), file.path))
-    formatted = format(filepath)
-    print.log(formatted) if formatted
-    cb(null, file)
+  return map(function(file, cb) {
+    const filepath = magenta(path.relative(process.cwd(), file.path));
+    const formatted = format(filepath);
+    if (formatted) { print.log(formatted); }
+    return cb(null, file);
+  });
+};
 
-print.log = log
-module.exports = print
+print.log = log;
+module.exports = print;
