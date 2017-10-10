867b4d32676cc6009274a1cb01af2dc0c8302ac5
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2017-10-10 16:30:10 -0700

    Renames to .ts

---
+++ src/gulp-print.ts
@@ -0,0 +1,25 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const map = require('map-stream');
+const path = require('path');
+const {log, colors} = require('gulp-util');
+
+const {green, magenta} = colors;
+
+var print = function(format) {
+  if (format == null) { format = filepath => filepath; }
+
+  return map(function(file, cb) {
+    const filepath = magenta(path.relative(process.cwd(), file.path));
+    const formatted = format(filepath);
+    if (formatted) { print.log(formatted); }
+    return cb(null, file);
+  });
+};
+
+print.log = log;
+module.exports = print;
