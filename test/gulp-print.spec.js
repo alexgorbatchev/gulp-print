30c7b77644cadeb208ce3fae133739b9ecf1ff4f
decaffeinate <alex.gorbatchev@gmail.com>
2018-01-31 10:36:06 -0800

    decaffeinate: Convert gulp-print.spec.coffee to JS

--- test/gulp-print.spec.js
+++ test/gulp-print.spec.js
@@ -1,49 +1,56 @@
-require 'coffee-errors'
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+require('coffee-errors');
 
-path = require 'path'
-chai = require 'chai'
-sinon = require 'sinon'
-Vinyl = require 'vinyl'
-colors = require 'ansi-colors'
-# using compiled JavaScript file here to be sure module works
-print = require '../lib/gulp-print.js'
+const path = require('path');
+const chai = require('chai');
+const sinon = require('sinon');
+const Vinyl = require('vinyl');
+const colors = require('ansi-colors');
+// using compiled JavaScript file here to be sure module works
+const print = require('../lib/gulp-print.js');
 
-expect = chai.expect
-chai.use require 'sinon-chai'
+const { expect } = chai;
+chai.use(require('sinon-chai'));
 
-describe 'gulp-print', ->
-  beforeEach ->
-    sinon.stub print, 'log'
-    return
+describe('gulp-print', function() {
+  beforeEach(function() {
+    sinon.stub(print, 'log');
+  });
 
-  afterEach ->
-    print.log.restore()
-    return
+  afterEach(function() {
+    print.log.restore();
+  });
 
-  describe 'passing formatting function', ->
-    it 'logs file path using default formatter', (done) ->
-      stream = print()
-      filepath = path.join process.cwd(), 'foo/bar.js'
+  return describe('passing formatting function', function() {
+    it('logs file path using default formatter', function(done) {
+      const stream = print();
+      const filepath = path.join(process.cwd(), 'foo/bar.js');
 
-      stream.on 'end', ->
-        expect(print.log).to.have.been.calledWith colors.magenta path.relative process.cwd(), filepath
-        done()
-        return
+      stream.on('end', function() {
+        expect(print.log).to.have.been.calledWith(colors.magenta(path.relative(process.cwd(), filepath)));
+        done();
+      });
 
-      stream.write new Vinyl path: filepath
-      stream.end()
-      return
+      stream.write(new Vinyl({path: filepath}));
+      stream.end();
+    });
 
-    it 'logs file paths using custom formatter', (done) ->
-      stream = print (filepath) -> "Hello #{filepath}"
-      filepath = path.join process.cwd(), 'foo/bar.js'
+    return it('logs file paths using custom formatter', function(done) {
+      const stream = print(filepath => `Hello ${filepath}`);
+      const filepath = path.join(process.cwd(), 'foo/bar.js');
 
-      stream.on 'end', ->
-        expect(print.log).to.have.been.calledWith "Hello #{colors.magenta path.relative process.cwd(), filepath}"
-        done()
-        return
+      stream.on('end', function() {
+        expect(print.log).to.have.been.calledWith(`Hello ${colors.magenta(path.relative(process.cwd(), filepath))}`);
+        done();
+      });
 
-      stream.write new Vinyl path: filepath
-      stream.end()
-      return
+      stream.write(new Vinyl({path: filepath}));
+      stream.end();
+    });
+  });
+});
 
